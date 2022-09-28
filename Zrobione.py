51b144f159d3c28a52f34e921d2196bc1fd17a42
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-09-28 09:02:21 +0200

    Add files via upload
    Zrobiona karta pracy 1

---
+++ Zrobione.py
@@ -0,0 +1,29 @@
+# Zad 1
+# a=int(input("podaj liczbe a:"))
+# b=int(input("podaj liczbe b:"))
+# print(a**2 + b**2)
+# Zad 2
+# a=int(input("podaj liczbe a:"))
+# b=int(input("podaj liczbe b:"))
+# print((a+b)**2)
+# Zad 3
+# a=int(input("podaj liczbe a:"))
+# b=int(input("podaj liczbe b:"))
+# print((a-b)**3)
+# Zad 4
+# a=int(input("podaj liczbe a:"))
+# b=int(input("podaj liczbe b:"))
+# c=int(input("podaj liczbe c:"))
+# print(a*b*c)
+# Zad 5
+# a=int(input("podaj liczbe a:"))
+# b=int(input("podaj liczbe b:"))
+# print(2*(a+b)/5)
+# print((a+b)*0.4)
+# Zad 6
+# a=int(input("podaj cenne netto:"))
+# print(a/1.23)
+# Zad 7
+# a=int(input("podaj liczbe a:"))
+# b=int(input("podaj liczbe b:"))
+# print(a%b)
